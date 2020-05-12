17cf39755eacb6759c7615f554b31800db63c1ad
van0k13 <kazak13v@gmail.com>
2020-05-12 20:44:26 +0300

    cards\cardsDeck refactoring

--- src/ui/features/uniCardsFeatures/uniCardsDeck/uniCardsDeck.tsx
+++ src/ui/features/uniCardsFeatures/uniCardsDeck/uniCardsDeck.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import styles from './uniCardsDeck.module.css'
+import Input from "../../../common/input/Input";
+import Button from "../../../common/button/Button";
 
 
 const UniCardsDeck = () => {
   return (
     <div className={styles.uniCardsWrapper}>
       <h2>UniCardsDeck</h2>
+        <Input inputPlaceholder={'item name'}/>
+        <Button buttonName={'search'}/>
+        <div className={styles.mainListWrapper}>
+            <Button buttonName={'add new Item'}/>
+            <div className={styles.mainList}>
+                <div className={styles.itemName}>Item name</div>
+                <div className={styles.itemScore}>Item score</div>
+                <div className={styles.buttonsInTheList}>
+                    <Button buttonName={'edit'}/>
+                    <Button buttonName={'delete'}/>
+                </div>
+            </div>
+        </div>
     </div>
   );
 };
