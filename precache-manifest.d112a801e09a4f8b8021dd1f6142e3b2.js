07ddd294495a7cca6e468ad3163de2688c066119
van0k13 <kazak13v@gmail.com>
2020-05-15 17:21:15 +0300

    Updates

---
+++ precache-manifest.d112a801e09a4f8b8021dd1f6142e3b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d979916d9afba3d759b9d3750473a7b",
+    "url": "/kartonki/index.html"
+  },
+  {
+    "revision": "0709a61ef6526f0759e0",
+    "url": "/kartonki/static/css/main.decd774f.chunk.css"
+  },
+  {
+    "revision": "6768ee9d8319484cbb69",
+    "url": "/kartonki/static/js/2.3b14b38f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kartonki/static/js/2.3b14b38f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0709a61ef6526f0759e0",
+    "url": "/kartonki/static/js/main.8f07c90e.chunk.js"
+  },
+  {
+    "revision": "b74b25845d50ecee2532",
+    "url": "/kartonki/static/js/runtime-main.42d09206.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kartonki/precache-manifest.2ad2f59a7c75df5d1363f0c66603d3af.js"
+  "/kartonki/precache-manifest.d112a801e09a4f8b8021dd1f6142e3b2.js"
 );
 
 self.addEventListener('message', (event) => {
